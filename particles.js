e0af37fab0dd9f2ebb5cb93087c904eac9e17c57
iharooniqbal <harooniqbal.ahi@gmail.com>
2026-01-31 21:17:18 +0500

    my first commit

--- particles.js
+++ particles.js
@@ -131,3 +131,20 @@ card.addEventListener('mousemove', function(e) {
     ripple.remove();
   });
 });
+
+
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+  reveals.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
+    if (elementTop < windowHeight - 80) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
